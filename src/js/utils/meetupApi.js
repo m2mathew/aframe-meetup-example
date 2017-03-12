15695fc0b8360b477f35d5326f426cb75abb99b1
rondagdag <rlyle78@gmail.com>
2017-03-12 15:06:35 -0500

    update meet up

--- src/js/utils/meetupApi.js
+++ src/js/utils/meetupApi.js
@@ -2,8 +2,8 @@ import fetchJsonp from 'fetch-jsonp'
 import {List, Map} from 'immutable'
 
 const API_ROOT = 'https://api.meetup.com'
-const GET_MEMBERS_URL = API_ROOT + '/2/rsvps?offset=0&format=json&event_id=228082564&photo-host=public&page=100&fields=&order=event&desc=false&sig_id=45273132&sig=a75637600bca84224283fb5dc2afcb73bb5f8bee'
-
+//const GET_MEMBERS_URL = API_ROOT + '/2/rsvps?offset=0&format=json&event_id=233915902&photo-host=public&page=100&fields=&order=event&desc=false&sig_id=45273132&sig=a75637600bca84224283fb5dc2afcb73bb5f8bee'
+const GET_MEMBERS_URL = API_ROOT + '/2/rsvps?offset=0&format=json&event_id=233915902&photo-host=public&page=100&fields=&order=event&desc=false&sig_id=5660110&sig=a8aaefdb29e1fbb4fcc078ad45f7131d6c707432'
 export function getMembers() {
   return fetchJsonp(GET_MEMBERS_URL)
     .then(response =>
